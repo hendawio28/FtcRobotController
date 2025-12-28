dd2b2e37513582f04a7ec95375d259c9f3021c66
UBA-RI <hendawio28@barringtonschools.org>
2025-12-28 18:11:14 -0500

    Created TestBenchColorSensor and ColorSensorTest
    Played around with color sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisims.TestBenchColorSensor;
+@Disabled
+@TeleOp
+public class ColorSensorTest extends OpMode {
+    TestBenchColorSensor bench = new TestBenchColorSensor();
+    TestBenchColorSensor.DetectedColor detectedColor;
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        detectedColor = bench.getDetectedColor(telemetry);
+        telemetry.addData("Detected Color", detectedColor);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisims/TestBenchColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisims/TestBenchColorSensor.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.mechanisims;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class TestBenchColorSensor {
     NormalizedColorSensor colorSensor;
@@ -9,9 +12,38 @@ public class TestBenchColorSensor {
     public enum DetectedColor {
         PURPLE,
         GREEN,
-        UNKOWN
+        UNKOWNN
     }
+
     public void init(HardwareMap hwMap) {
-        colorSensor = hwMap.get(NormalizedColorSensor.class, )
+        colorSensor = hwMap.get(NormalizedColorSensor.class, "sensor_color_distance");
+        colorSensor.setGain(10);
+    }
+
+    public DetectedColor getDetectedColor(Telemetry telemetry) {
+        NormalizedRGBA colors = colorSensor.getNormalizedColors(); // returns 4 values Red Green Blue
+        // and Alpha/how much light is getting returned back
+        float normRed, normGreen, normBlue;
+        normRed = colors.red / colors.alpha;
+        normGreen = colors.green / colors.alpha;
+        normBlue = colors.blue / colors.alpha;
+
+        telemetry.addData("red", normRed);
+        telemetry.addData("green", normGreen);
+        telemetry.addData("blue", normBlue);
+
+        // TODO add if statements for specefic colors added
+        /*
+        red, green, blue
+        GREEN = <0.25, >0.75, <0.6
+        Purple = >0.9, >0.9 >0.9
+         */
+        if (normRed < 0.25 && normGreen > 0.75 && normBlue < 0.6) {
+            return DetectedColor.GREEN;
+        } else if (normRed > 0.9 && normGreen > 0.9 && normBlue > 0.9) {
+            return DetectedColor.PURPLE;
+        } else {
+            return DetectedColor.UNKOWNN;
+        }
     }
 }
